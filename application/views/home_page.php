a2129f0633c7980b8cd8527f110d4f6c7fa24d7d
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-10 22:59:28 +0000

    Created home page

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -1,4 +1,6 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<p>Hello world!</p>
+<img src = "<?=base_url('assets/img/logo_3d.png');?>" class=img-responsive' alt='3D HangMan Logo' id='main_logo'>
+<img src = "<?=base_url('assets/img/play.png');?>" class=img-responsive' alt='Play the game?'> 
+<img src = "<?=base_url('assets/img/scores.png');?>" class=img-responsive' alt='View high scores!'>
