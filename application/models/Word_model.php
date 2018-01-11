419b51060d4fb14c27ced1ff12a588ef4e010523
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-11 20:03:29 +0000

    Images and some comments

--- application/models/Word_model.php
+++ application/models/Word_model.php
@@ -1,10 +1,13 @@
 <?php
 class Word_model extends CI_Model {
 
+	/**
+	 * Fetches data from the Json file
+	**/
 	public function getWords()
 	{
 		$json = read_file('words/words.json');
-		$arr = json_decode($json,true);
+		$arr = json_decode($json,true); //true will return an array
 		return $arr;
-	}
+	}//end of method
 }//end of class
