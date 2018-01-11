ab57c324b07f74a42c765c5f1309dd57923ad288
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-11 22:58:14 +0000

    Preparing ajax controller method

--- application/controllers/Game.php
+++ application/controllers/Game.php
@@ -5,15 +5,33 @@ class Game extends CI_Controller {
 	public function __construct()
         {       
                 parent::__construct();
-		$this->load->helper('url');
+		$this->load->helper(array('url','form'));
         }
 
 	public function index()
 	{
+		$words['word'] = $this->loadWord();
 		$data['title'] = 'Play Hangman';
 		$this->load->view('header',$data);
-		$this->load->view('play_game');
+		$this->load->view('play_game',$words);
 		$this->load->view('footer');
+	}
+
+	public function userChoice()
+	{
+		error_log($this->input->post('choice'),0);
+	}
+
+	/**
+	 * Get the words and the description from the model
+	**/
+	private function loadWord()
+	{
+		$this->load->model('word_model');
+		$arr = $this->word_model->getWords();
+		$num2 = sizeof($arr);//take the size of the array as the upper limit
+		$wordIndex = rand(0,$num2-1); //return me an index between 0 and num2-1 (because of array index)
+		return $arr[$wordIndex]; //return a single word with the description
 	}
 }
 
