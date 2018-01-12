c7f43bf3ea0a33c3ad4eb7e80fb6bfa55579b94a
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-12 15:59:48 +0000

    Moved back js because it can\'t find php vars

--- application/views/footer.php
+++ application/views/footer.php
@@ -6,6 +6,31 @@
 
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<script type = 'text/javascript' src = "<?=base_url();?>assets/js/main.js"></script>
+
+<script type = 'text/javascript'>
+$(document).ready(function() {
+	$('.submit_form').click(function(e) {
+		var hostname = $(location).attr('host'); //not in use
+		var choice = $('#user_input').val();
+		if (choice.length > 0) {
+			$.ajax({
+				type: "POST",
+				url: "/3dhangman/game/userChoice",
+				//dataType: 'json',
+				data: {choice:choice},
+				success: function(result) {
+					if (result) {
+						$('#user_input').val('');
+						$( '#hang_image' ).attr("src","assets/img/level1.png");
+						$('.word').text(result);
+						console.log(result);
+					}
+				}
+			});//end fo ajax
+		} //end of input check
+	}); //end of submit_form click
+}); //end of document ready function
+
+</script>
 </body>
 </html>
