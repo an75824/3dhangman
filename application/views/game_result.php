c79cc01dd3204fa14cc2450e95120719ce49db6b
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-14 22:49:49 +0000

    Change img and print the result

--- application/views/game_result.php
+++ application/views/game_result.php
@@ -2,10 +2,17 @@
 if (isset($_SESSION['result']))
 {
 	echo $_SESSION['result'];
+?>
+	<script>
+		var img = "assets/img/level"+<?=$_SESSION['img'];?>+".png";
+		$('#user_input').val('');
+		$( '#hang_image' ).attr("src",img);
+	</script>
+<?php
 }
 
 if (isset($duplicate_char))
 {
 	echo " found duplicate character: $duplicate_char";
 }
-?>
+
