3fce84e4e902bc7f3d7f91efe179e8a9a05c1c1b
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-15 10:08:27 +0000

    Remove button after game is over

--- application/views/game_over.php
+++ application/views/game_over.php
@@ -4,5 +4,6 @@
 	var img = "assets/img/level"+<?=$_SESSION['img'];?>+".png";
 	$("#user_input").attr("disabled", "disabled");
 	$( '#hang_image' ).attr("src",img);
+	$('.submit_form').remove();
 </script>
 
