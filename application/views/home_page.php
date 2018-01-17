b905b82667156893f1d5516f3eccb9ae2c6eced2
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-17 22:51:59 +0000

    Changed the layout of the home page

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -2,9 +2,14 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <img src = "<?=base_url('assets/img/logo_3d.png');?>" class='img-responsive' alt='3D HangMan Logo' id='main_logo'>
-<div class="col-md-6">
-	<a href = "<?=base_url('game');?>"> <img src = "<?=base_url('assets/img/play.png');?>" class=img-responsive' alt='Play the game?'></a>
-</div>
-<div class="col-md-6">
-<img src = "<?=base_url('assets/img/scores.png');?>" class='img-responsive' alt='View high scores!'>
-</div>
+<center>
+<p>
+	<a href = "<?=base_url('game');?>">
+		<img src = "<?=base_url('assets/img/play.png');?>" class=img-responsive' alt='Play the game?'>
+	</a>
+</p>
+
+<a href = "<?=base_url('score/highScores');?>">
+	<img src = "<?=base_url('assets/img/scores.png');?>" class='img-responsive' alt='View high scores!'>
+</a>
+</center>
