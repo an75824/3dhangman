16d2eead98a7567f42b681c07149c73d31a70c58
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-17 19:00:12 +0000

    Modal for the scores

--- application/views/header.php
+++ application/views/header.php
@@ -80,6 +80,26 @@
   </div>
 </div>
 
+<!-- Modal for the score -->
+<div class="modal" id = 'modal_score' tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Results</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id = "results_modal_body">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" id = 'submit_full_word'>Submit</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
     <div class="container">
 
--- application/views/modal_results.php
+++ application/views/modal_results.php
@@ -1,1 +1,4 @@
-<p>Hello world!</p>
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<?=var_dump($scores);?>
