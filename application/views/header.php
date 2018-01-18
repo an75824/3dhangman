a0e63f215b4d37afab193e0f4387c1e4538494d7
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-18 09:59:43 +0000

    Changed the paths to match our own

--- application/views/header.php
+++ application/views/header.php
@@ -20,6 +20,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
 	<link rel="stylesheet" href="<?=base_url('assets/css/main.css');?>" />
+	<link rel="stylesheet" href="<?=base_url('assets/css/jquery.Wload.css');?>" />
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
