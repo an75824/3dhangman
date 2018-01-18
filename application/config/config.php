0b234328b96c7bdc32214d4ee1895e11fda161b1
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-18 22:18:04 +0000

    Changed the base_url and the sess_save_path for storing sessions

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$local_ip = $_SERVER['SERVER_ADDR']; //does not work with vagrant
-$config['base_url'] = 'http://'.$local_ip.'/3dhangman/';
+$config['base_url'] = 'http://127.0.0.1/3dhangman/';
 
 /*
 |--------------------------------------------------------------------------
@@ -381,7 +380,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = '/var/ci_sessions/';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
