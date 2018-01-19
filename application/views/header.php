0f6ba336734fe73d3c91375dd8ffadb1fb19f12d
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-19 09:42:46 +0000

    Favicon

--- application/views/header.php
+++ application/views/header.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name='description' content='A Hangman game'>
     <meta name='author' content='Antony'>
-
+    <link rel="icon" type="image/png" href="<?=base_url('favicon.ico');?>" />
     <title><?=$title;?></title>
 
 	<noscript>
