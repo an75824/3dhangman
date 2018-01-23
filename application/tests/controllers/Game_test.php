5457adf6743ac7dd65fae79d7da3d545ba0e5008
antony13 <nanosa@uni.coventry.ac.uk>
2018-01-23 10:38:46 +0000

    Some explanation about the userChoice method

--- application/tests/controllers/Game_test.php
+++ application/tests/controllers/Game_test.php
@@ -11,7 +11,7 @@ class Base_test extends TestCase {
 	public function test_userChoice_fail()
 	{
 		$output = $this->request('POST','Game/userChoice',['choice'=>'hello']);
-		$expetcted = $this->assertContains('hello',$output);
+		$expetcted = $this->assertContains('h',$output); //it will also fail if 'h' is 'hello'
 	}
 
 }//end of class
